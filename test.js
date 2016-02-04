eee00eac6c95e0730b7d220b0ef6e6201523d76e
Marc Bachmann <marc.brookman@gmail.com>
2016-02-04 17:29:14 +0100

    Add test for HTTP-Date support

--- test.js
+++ test.js
@@ -6,8 +6,11 @@ assert.equal(warning.length, 1)
 var w = warning[0]
 assert.deepEqual(w, {code: '199', agent: 'agent', message: 'Error message', date: '2015-01-01'})
 
-
 var warnings = parse('199 agent "Error message" "2015", 299 agent2 "Error message 2 with \" quote" "2016"')
 assert.equal(warnings.length, 2)
 assert.deepEqual(warnings[0], {code: '199', agent: 'agent', message: 'Error message', date: '2015'})
 assert.deepEqual(warnings[1], {code: '299', agent: 'agent2', message: 'Error message 2 with \" quote', date: '2016'})
+
+// HTTP-Date format support https://tools.ietf.org/html/rfc7231#section-7.1.1.1
+var httpDate = parse('199 agent "Error message" "Sun, 06 Nov 1994 08:49:37 GMT"')
+assert.deepEqual(httpDate[0], {code: '199', agent: 'agent', message: 'Error message', date: 'Sun, 06 Nov 1994 08:49:37 GMT'})
